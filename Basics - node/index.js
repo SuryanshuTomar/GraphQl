f69d900523eb32dafcad2c84e3c6087a0f2694f9
Suryanshu Tomar <suryanshutomar123@outlook.com>
2023-11-05 15:42:32 +0530

    add: typedefs to the server config

--- Basics - node/index.js
+++ Basics - node/index.js
@@ -1,6 +1,9 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+// graphql types
+import { typeDefs } from "./schema";
+
 // Apollo server takes an object which takes two arguments
 // 1. Typedefs -> These are definition of different datatypes we wanna expose in our graph.
 // 2. resolvers -> resolvers functions that determines how we respond to different queries on data on the graph.
@@ -8,6 +11,10 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 // Server Setup -
 const server = new ApolloServer({
 	// Typedefs
+	typeDefs,
+
+	// Resolvers
+	// resolvers,
 });
 
 // Start the server
